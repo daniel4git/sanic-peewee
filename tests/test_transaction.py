2524d115efb713bd208cc5f61449ee2f54a2d5a6
HUANG SIZHE <hsz1273327@gmail.com>
2017-04-08 13:01:44 +0800

    update
    update

--- tests/test_transaction.py
+++ tests/test_transaction.py
@@ -5,22 +5,46 @@
 @Date:   06-Apr-2017
 @Email:  hsz1273327@gmail.com
 @Last modified by:   huangsizhe
-@Last modified time: 07-Apr-2017
+@Last modified time: 08-Apr-2017
 @License: MIT
 @Description:
 """
 
 
 
-from sanic_peewee import Peewee
+import asyncio
 
 import unittest
+from tests.models import MysqlTestModelAlpha, MysqlTestModelBeta, MysqlUUIDTestModel
+from tests import peewee_mysql
 
-class TestStringMethods(unittest.TestCase):
+
+class TestTransaction(unittest.TestCase):
+    models = [MysqlTestModelAlpha, MysqlTestModelBeta, MysqlUUIDTestModel]
+    db = peewee_mysql
+    @classmethod
+    def setUpClass(cls, *args, **kwargs):
+        """Configure database managers, create test tables.
+        """
+        cls.loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(cls.loop)
+    @classmethod
+    def tearDownClass(cls, *args, **kwargs):
+        """Remove all test tables and close connections.
+        """
+
+        cls.loop.run_until_complete(cls.db.close())
+        cls.loop.close()
+
     def setUp(self):
-        pass
-
-
+        self.db.create_tables(self.models,safe=True)
 
     def tearDown(self):
-        pass
+        self.db.drop_tables(self.models,safe=True)
+
+    def run_with_databases(self, test,):
+        """Run test coroutine against available databases.
+        """
+        self.loop.run_until_complete(test())
+    def test_atomic():
+        
